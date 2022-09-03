e3aa9a129b0313f068b9eb15025c327528ee64c3
Suyeon Lee <suyeon9477@gmail.com>
2022-09-03 19:02:29 +0900

    추가: 1259 팰린드롬수 solution.py README.md

---
+++ Baekjoon/String/1259  팰린드롬수/solution.py
@@ -0,0 +1,11 @@
+import math
+answer = []
+
+while True:
+    n = input()
+    if n == '0': break
+    elif n[0:len(n)//2] == n[:math.ceil(len(n)/2)-1:-1]: #ceil은 3.5 -> 4 위의 숫자로 올려줌
+        answer.append('yes')
+    else: answer.append('no')
+
+for i in answer: print(i)
