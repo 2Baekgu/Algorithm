ff6db457d958fc8d11515a27c62d829304561c16
Suyeon Lee <suyeon9477@gmail.com>
2022-09-21 12:56:12 +0900

    수정: 피보나치수계산 solution.cpp

--- 알고리즘/피보나치수계산/solution.cpp
+++ 알고리즘/피보나치수계산/solution.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void Fibo(int n);
+int Fibo(int n);
 int main()
 {
  int numTestCases;
@@ -9,20 +9,15 @@ int main()
  {
  int num;
  scanf("%d", &num);
- Fibo(num);
+ printf("%d\n", Fibo(num));
  }
  return 0;
 }
 /* 피보나치 함수 */
-void Fibo(int n)
+int Fibo(int n)
 {
- int first = 0;
- int second = 1;
- for(int i = 0; i<n; i++)
- {
-    int tmp = first;
-    first = second;
-    second = tmp + second;
- }
- printf("%d\n",first);
+    if (n <= 1) 
+        return n;
+    else
+        return Fibo(n-1) + Fibo(n-2);
 }
