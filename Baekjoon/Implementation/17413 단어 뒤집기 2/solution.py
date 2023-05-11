14c938aeaf5997cfdb6423ded5904e7a02d64e32
Suyeon Lee <suyeon9477@gmail.com>
2023-05-11 21:22:42 +0900

    추가: 17413 단어 뒤집기 solution.py README.md

---
+++ Baekjoon/Implementation/17413 단어 뒤집기 2/solution.py
@@ -0,0 +1,24 @@
+s = input()
+word = []
+reverse = True
+at = 0
+for i in range(len(s)):
+    if s[i] == '<' :
+        word.append(s[i])
+        reverse = False
+    elif s[i] == '>':
+        word.append(s[i])
+        reverse = True
+        at = i+1
+    elif s[i] == ' ':
+        word.append(s[i])
+        at = i+1
+    else:
+        if reverse == True:
+            word.insert(at, s[i])
+        else:
+            word.append(s[i])
+
+for i in word: print(i, end='')
+
+    
